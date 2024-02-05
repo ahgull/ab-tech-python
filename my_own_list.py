c0c0ad4d8222e4f8bbf4da0c6dba337b1a144bc5
ahgull <aliciajet@gmail.com>
2024-02-04 21:28:06 -0500

    assignment 3-3

---
+++ my_own_list.py
@@ -0,0 +1,7 @@
+transportation = ['bicycle', 'horse', 'car']
+message = f"I would like to get around using a {transportation[0]}."
+print(message)
+message = f"I would like to get around using a {transportation[1]}."
+print(message)
+message = f"I would like to get around using a {transportation[2]}."
+print(message)
