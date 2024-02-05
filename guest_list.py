93b8093088154a8e5c473f676dd4e9f9dfd72fcc
ahgull <aliciajet@gmail.com>
2024-02-04 21:38:04 -0500

    init guest_list

---
+++ guest_list.py
@@ -0,0 +1,7 @@
+guests = ['jane goodall', 'henry cavill', 'brandon sanderson']
+message = f'Dear {guests[0].title()}, I would be honored if you would join me for a dinner party on a date to be determined in the future.'
+print(message)
+message = f'Dear {guests[1].title()}, I would be honored if you would join me for a dinner party on a date to be determined in the future.'
+print(message)
+message = f'Dear {guests[2].title()}, I would be honored if you would join me for a dinner party on a date to be determined in the future.'
+print(message)
