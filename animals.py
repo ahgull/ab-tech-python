609685ea8112f5cc29ad21c197dcdab9f283aaa8
ahgull <aliciajet@gmail.com>
2024-02-07 18:25:05 -0500

    init animals

---
+++ animals.py
@@ -0,0 +1,4 @@
+animals = ['deer', 'raccoon', 'possum']
+for animal in animals:
+    print(f"A {animal} is a common backyard visitor!")
+print('These animals are all easy to find in your own backyard!')
