c46bda52a2f48e1c15669d869279790ac5145c90
ahgull <aliciajet@gmail.com>
2024-02-09 17:48:09 -0500

    init slices

---
+++ slices.py
@@ -0,0 +1,15 @@
+guests = ['jane goodall', 'henry cavill', 'brandon sanderson']
+guests.insert(0, 'frank herbert')
+guests.insert(2, 'neil gaiman')
+guests.append('bernard cornwell')
+guests.sort()
+
+print("The first three items on the list are:")
+for guest in guests[:3]:
+    print(guest.title())
+print("Three items in the middle of the list the list are:")
+for guest in guests[2:5]:
+    print(guest.title())
+print("The last three items on the list are:")
+for guest in guests[3:]:
+    print(guest.title())
