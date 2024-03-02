ea0947c8f6761c41a75f1089e6f2336dceb3f556
ahgull <aliciajet@gmail.com>
2024-03-02 14:09:37 -0500

    init multiples_of_ten

---
+++ multiples_of_ten.py
@@ -0,0 +1,6 @@
+number = input("Enter a number, and I'll tell you if it's a multiple of ten!")
+number = int(number)
+if number % 10 ==0:
+    print(f"\nThis number is a multiple of ten!")
+else:
+    print(f"\nThis number is not a multiple of ten.")
