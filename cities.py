b22c5bc5c7181be5676f0829425cfae7b80c50f7
ahgull <aliciajet@gmail.com>
2024-03-10 13:14:23 -0400

    init cities

---
+++ cities.py
@@ -0,0 +1,5 @@
+def describe_city(city_name, country_name='belgium'):
+    print(f"\n{city_name.title()} is in {country_name.title()}.")
+describe_city('kortrijk')
+describe_city('rio', 'brazil')
+describe_city('cairo', 'egypt')
